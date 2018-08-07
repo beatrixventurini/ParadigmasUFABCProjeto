52c59d5d7d69d43e01d915343d88af2315e3d2fa
Yago Sorrilha <yagosorrilha@hotmail.com>
2018-08-07 20:19:35 -0300

    Alterado condição de parada da função splitDataset-i

--- Classifier.rkt
+++ Classifier.rkt
@@ -1,10 +1,10 @@
 #lang racket
 
 ;função que separa um conjunto de dados de treino, de acordo com a porcentagem
-;definida, p. ex. 0.67 (67%)
+;definida, p. ex. 0.70 (70%)
 
 (define (splitDataset dataset splitRatio)
   (define trainSize (exact-floor (* (length dataset) splitRatio)))
   (let splitDataset-i ([trainSet '()])
-    (cond [(> (length trainSet) trainSize) trainSet]
+    (cond [(>= (length trainSet) trainSize) trainSet]
           [else (splitDataset-i (cons (list-ref dataset (random 0 trainSize)) trainSet))])))
