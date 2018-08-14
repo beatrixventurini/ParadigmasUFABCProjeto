7ee4baf9386602ebf55ff8d5e65f2a84f6b850f2
Yago Sorrilha <yagosorrilha@hotmail.com>
2018-08-14 01:51:25 -0300

    Adicionado função summarize no NB

--- nb.rkt
+++ nb.rkt
@@ -20,6 +20,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Testes:
 ;(define x (list '("água" "suco" "refri" 0) '("cerveja" "vodka" "vinho" 1) '("whisky" "licor" "rum" 1)))
+(define x (list '(1 2 0) '(1 2 0) '(1 2 1)))
 ;(separateByClass_0 x)
 ;(separateByClass_1 x)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -32,5 +33,14 @@
      (exp (/ (* -1 (expt (- x media) 2)) (* 2 (expt stdev 2))))))
 ;;;;;;;;;Teste;;;;;;;;;;;;;;;;;;;
 ;x= 71.5, média=73. desvio padrao = 6.2, probabilidade deve ser aprox. 0.062489
-(calcProbab 71.5 73 6.2)
- 
+;(calcProbab 71.5 73 6.2)
+
+;Função que calcula a média e o desvio padrão de todos os atributos de uma mesma posição dentro das sub-listas
+;O retorno são pares onde o car do par é a média e o cdr o desvio padrão
+(define (summarize dataset)
+  (let loop([lst dataset] [summary null])
+  (cond [(null? (car (map cdr lst))) summary]
+        [else (loop (map cdr lst) (list summary (cons (mean (map car lst)) (stddev (map car lst)))))])))
+
+
+
